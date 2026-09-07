0fcc6103da1c33889495a41c2ee5c68f5178babe
NEHA <nehaersavadla6@gmail.com>
2026-09-07 23:31:04 +0200

    chore: add Capacitor Android platform

---
+++ android/app/src/main/java/com/mummabear/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.mummabear.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mummabear.app',
+  appName: 'Mumma Bear',
+  webDir: 'www'
+};
+
+export default config;
